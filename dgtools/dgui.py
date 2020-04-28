0b19b862f239f2009b420a932ec18825760d734c
Athanasios Anastasiou <athanastasiou@gmail.com>
2020-04-28 20:14:34 +0100

    Basic parameter validation and feedback for dgui

--- dgtools/dgui.py
+++ dgtools/dgui.py
@@ -29,37 +29,41 @@ def get_symbols_parser():
     identifier = pyparsing.Regex("[a-zA-Z_][0-9A-Za-z_]*")
     positive_integer = pyparsing.Regex("[0-9]+")
     numeric_param = pyparsing.Suppress(":") + positive_integer
-    symbol_record = pyparsing.Group(identifier("id") + pyparsing.Optional(numeric_param("len") + pyparsing.Optional(numeric_param("offset"))))
-    symbol_records = pyparsing.Empty() ^ pyparsing.delimitedList(symbol_record)    
+    
+    symbol_record = pyparsing.Group(identifier("id") + 
+                    pyparsing.Optional(numeric_param("len") + 
+                    pyparsing.Optional(numeric_param("offset"))))
+    symbol_records = pyparsing.delimitedList(symbol_record)    
     return symbol_records
 
 
 
 class MessageBox(urwid.WidgetWrap):
+    """
+    Presents a standard OK, Cancel message box.
+    """
+    # Signals emmitted by this widget
     signals=["close"]
     def __init__(self, message):
-        ok_cancel = urwid.GridFlow([urwid.Button("OK".center(15), on_press=self.on_ok), 
-                                    urwid.Button("Cancel".center(15), on_press=self.on_cancel)],20,4,1,"center")
-        mb_widget = urwid.LineBox(urwid.ListBox([urwid.Text(message), urwid.Divider(), ok_cancel]))
+        """
+        Sets up the message box
+        """
+        ok_cancel = urwid.GridFlow([urwid.AttrMap(urwid.Button("OK".center(15), on_press=self.on_ok),
+                                                  "dialog_button_plain",
+                                                  "dialog_button_focused"),],
+                                   20,4,1,"center")
+        mb_widget = urwid.AttrMap(urwid.LineBox(
+                                                urwid.ListBox([urwid.Text("\n".join(message)), 
+                                                               urwid.Divider("\u2015"), 
+                                                               ok_cancel])), "bg")
         
         super().__init__(mb_widget)
         
     def on_ok(self, a_b):
         self._emit("close")
         
-    def on_cancel(self, a_b):
-        self._emit("close")
-
-
-class ValidationFeedbackModalBox(urwid.PopUpLauncher):
-    def __init__(self, ovr_window, validation_messages):
-        # Build the dialog box
-        # Switch the loop's dialog box
-        # Redraw the screen
-        super().init(urwid.Overlay())
-
     
-class ModalDialogBox(urwid.WidgetWrap):
+class ModalDialogBox(urwid.PopUpLauncher):
     """
     A standard modal dialog box that handles all user interaction.
     
@@ -88,19 +92,35 @@ class ModalDialogBox(urwid.WidgetWrap):
         self._in_interactive_mode = urwid.CheckBox("Interactive mode")
         self._maximum_cycles_to_run = urwid.IntEdit("Maximum cycles to run:",200)
         self._extra_syms = urwid.Edit("Extra symbols to track:")
-        ok_cancel = urwid.GridFlow([urwid.AttrMap(urwid.Button("OK".center(15),on_press = self._on_ok),"dialog_button_plain","dialog_button_focused"), 
-                                    urwid.AttrMap(urwid.Button("Cancel".center(15), on_press = self._on_cancel),"dialog_button_plain","dialog_button_focused")],20,4,1,"center")
+        
+        ok_cancel = urwid.GridFlow([urwid.AttrMap(urwid.Button("OK".center(15),
+                                                               on_press = self._on_ok),
+                                                  "dialog_button_plain",
+                                                  "dialog_button_focused"), 
+                                    urwid.AttrMap(urwid.Button("Cancel".center(15), 
+                                                               on_press = self._on_cancel),
+                                                  "dialog_button_plain",
+                                                  "dialog_button_focused")],
+                                    20,4,1,"center")
+                                    
         final_widget = urwid.AttrMap(urwid.LineBox(urwid.ListBox([
-                                                   urwid.AttrMap(urwid.Text("Arrow keys navigate the menu. <Enter> or <Space> marks the checkboxes. <Esc> cancels everything and returns to prompt."),"dialog_help"),
+                                                   urwid.AttrMap(urwid.Text("Arrow keys navigate the menu. <Enter> "
+                                                                            "or <Space> marks the checkboxes. <Esc> "
+                                                                            "cancels everything and returns to "
+                                                                            "prompt."),
+                                                                 "dialog_help"),
                                                    urwid.AttrMap(urwid.Text("Input/Output"),"dialog_section"),
-                                                   urwid.AttrMap(self._in_file,"dialog_plain","dialog_focused"),
+                                                   urwid.AttrMap(urwid.WidgetDisable(self._in_file),
+                                                                 "dialog_plain", "dialog_focused"),
                                                    urwid.AttrMap(self._out_file,"dialog_plain","dialog_focused"),
                                                    urwid.AttrMap(self._trace_file,"dialog_plain","dialog_focused"),
                                                    urwid.AttrMap(urwid.Text("Simulation parameters"),"dialog_section"),     
                                                    urwid.AttrMap(self._trace_title,"dialog_plain","dialog_focused"), 
                                                    urwid.AttrMap(self._with_mem_dump,"dialog_plain","dialog_focused"), 
-                                                   urwid.AttrMap(self._in_interactive_mode,"dialog_plain","dialog_focused"), 
-                                                   urwid.AttrMap(self._maximum_cycles_to_run,"dialog_plain","dialog_focused"), 
+                                                   urwid.AttrMap(self._in_interactive_mode,
+                                                                 "dialog_plain","dialog_focused"), 
+                                                   urwid.AttrMap(self._maximum_cycles_to_run,
+                                                                 "dialog_plain","dialog_focused"), 
                                                    urwid.AttrMap(self._extra_syms,"dialog_plain","dialog_focused"),
                                                    urwid.Divider("\u2015"),
                                                    ok_cancel]), title="Dgtools Compilation Parameters"),"dialog_plain")
@@ -110,28 +130,39 @@ class ModalDialogBox(urwid.WidgetWrap):
     def _on_ok(self, a_button):
         validation_messages = []
         # Check the format of extras
-        try:
-            get_symbols_parser().parseString(self.extra_symbols)
-        except pyparsing.ParseException:
-            validation_messages.append("Please check extra symbols passed.")
+        if len(self._extra_syms.edit_text)>0:
+            try:
+                get_symbols_parser().parseString(self._extra_syms.edit_text)
+            except pyparsing.ParseException:
+                validation_messages.append("* Please check extra symbols passed.")
         # Check that output directories exist
         output_file_dir = os.path.split(self.output_file)[0]
         if not os.path.isdir(output_file_dir):
-            validation_messages.append(f"Path {output_file_dir} does not exist")
+            validation_messages.append(f"* Binary file path {output_file_dir} does not exist")
 
         output_trace_file_dir = os.path.split(self.output_trace_file)[0]
+        
         if not os.path.isdir(output_trace_file_dir):
-            validation_messages.append(f"Path {output_trace_file_dir} does not exist")
+            validation_messages.append(f"* Trace HTML file path {output_trace_file_dir} does not exist")
 
         if len(validation_messages)>0:
-            pass
+            self._validation_messages = validation_messages
+            self.open_pop_up()
         else:
             self._was_ok = True
             raise urwid.ExitMainLoop()
                 
     def _on_cancel(self, a_button):
         raise urwid.ExitMainLoop()
-    
+
+    def get_pop_up_parameters(self):
+        return {"left":-5, "top":1, "overlay_width":80, "overlay_height":7}
+
+    def create_pop_up(self):
+        pop_up = MessageBox(self._validation_messages)
+        urwid.connect_signal(pop_up, 'close', lambda button: self.close_pop_up())
+        return pop_up
+
     @property
     def closed_ok(self):
         return self._was_ok
