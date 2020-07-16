c5183ba0e5fbb8a4d353490771ff2c1ebf593cc0
Athanasios Anastasiou <athanastasiou@gmail.com>
2020-07-16 22:21:48 +0100

    First working version of the brainfuck to asm compiler

--- dg_asm_examples/brainfuck/brainfuck_to_asm.py
+++ dg_asm_examples/brainfuck/brainfuck_to_asm.py
@@ -1,22 +1,80 @@
+#!/usr/bin/env python
 import pyparsing
+import random
+import click
+import sys
+import os
 
 def get_bf_parser():
-    bf_inc_data_p = pyparsing.Group(pyparsing.Suppress(">"))("INC_DP")
-    bf_dec_data_p = pyparsing.Group(pyparsing.Suppress("<"))("DEC_DP")
-    bf_inc_data_v = pyparsing.Group(pyparsing.Suppress("+"))("INC_DV")
-    bf_dec_data_v = pyparsing.Group(pyparsing.Suppress("-"))("DEC_DV")
-    bf_output = pyparsing.Group(pyparsing.Suppress("."))("OUT_DV")
-    bf_input = pyparsing.Group(pyparsing.Suppress(","))("IN_DV")
+    """
+    Returns the complete brainfuck parser --> Digirule ASM.
+    """
+    def _get_label_tag(tag_chars="0123456789", N=12):
+        return "".join([tag_chars[random.randint(0,len(tag_chars)-1)] for n in range(N)])
+        
+    def inc_dp(s, loc, toks):
+        reps = len(toks[0][0])
+        if reps>1:
+            return f"COPYRA dp\nADDLA {reps}\nCOPYAR dp\n" 
+        else:
+            return f"INCR dp\n"
+    
+    def dec_dp(s, loc, toks):
+        reps = len(toks[0][0])
+        if reps>1:
+            return f"COPYRA dp\nSUBLA {reps}\nCOPYAR dp\n" 
+        else:
+            return f"DECR dp\n"
+            
+    def inc_dv(s, loc, toks):
+        return f"COPYIA dp\nADDLA {len(toks[0][0])}\nCOPYAI dp\n" 
+        
+    def dec_dv(s, loc, toks):
+        return f"COPYIA dp\nADDLA {len(toks[0][0])}\nCOPYAI dp\n"        
+    
+    def out_dv(s, loc, toks):
+        return f"COPYIR dp out_dev\n"
+                
+    def in_dv(s, loc, toks):
+        return f"COPYRI in_dev dp\n"
+        
+    def iteration_block(s, loc, toks):
+        label_tag = _get_label_tag()
+        return f"label_{label_tag}:\n{''.join(toks[0][1:-1])}COPYIA dp\nBCRSC zero_bit status_reg\nJUMP label_{label_tag}\n"
+        
+    def emit_asm(s, loc, toks):
+        return f".EQU status_reg=252\n.EQU in_dev=253\n.EQU out_dev=255\n.EQU zero_bit=0\nCOPYLR tape dp\nstart_program:\n" \
+               f"{''.join([m for m in toks])}HALT\ndp:\n.DB 0\ntape:\n.DB 0"
+        
+    bf_inc_data_p = pyparsing.Group(pyparsing.Regex("[>]+"))("INC_DP").setParseAction(inc_dp)
+    bf_dec_data_p = pyparsing.Group(pyparsing.Regex("[<]+"))("DEC_DP").setParseAction(dec_dp)
+    bf_inc_data_v = pyparsing.Group(pyparsing.Regex("[+]+"))("INC_DV").setParseAction(inc_dv)
+    bf_dec_data_v = pyparsing.Group(pyparsing.Regex("[-]+"))("DEC_DV").setParseAction(dec_dv)
+    bf_output = pyparsing.Group(pyparsing.Suppress("."))("OUT_DV").setParseAction(out_dv)
+    bf_input = pyparsing.Group(pyparsing.Suppress(","))("IN_DV").setParseAction(in_dv)
     bf_jz = pyparsing.Group(pyparsing.Suppress("["))("JZ")
     bf_jnz = pyparsing.Group(pyparsing.Suppress("]"))("JNZ")
     bf_statement = pyparsing.Forward()
     bf_statement << (bf_inc_data_p ^ bf_dec_data_p ^ bf_inc_data_v ^ bf_dec_data_v ^ bf_output ^ bf_input ^  \
-                    pyparsing.Group(bf_jz + pyparsing.ZeroOrMore(bf_statement) + bf_jnz)("BLOCK"))
-    bf_program = pyparsing.OneOrMore(bf_statement)
+                    pyparsing.Group(bf_jz + pyparsing.ZeroOrMore(bf_statement) + bf_jnz)("ITERATION").setParseAction(iteration_block))
+    bf_program = pyparsing.OneOrMore(bf_statement).setParseAction(emit_asm)
     return bf_program
     
+    
+@click.command()
+@click.argument("input_file", type=click.File("rt"))
+def main(input_file):
+    bf2asm_parser = get_bf_parser()
+    try:
+        dg_asm_text = bf2asm_parser.parseString(input_file.read(), parseAll=True)
+        sys.stdout.write(dg_asm_text[0])
+    except pyparsing.ParseException as pe:
+        print(str(pe))
+        sys.exit(-1)
+
 if __name__ == "__main__":
-    program = "+>>+++++++++[<<[>++<-]>[<+>-]>-]<<"
-    z = get_bf_parser()
-    v = z.parseString(program)
+    main()
+    # program = "+>>+++++++++[<<[>++<-]>[<+>-]>-]<<"
+    
+        
     
