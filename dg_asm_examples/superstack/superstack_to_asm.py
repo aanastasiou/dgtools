6d64c53b6de09896e52038c98ae0525d30ce8a93
Athanasios Anastasiou <athanastasiou@gmail.com>
2020-07-18 22:54:16 +0100

    First working version of the superstack compiler

--- dg_asm_examples/superstack/superstack_to_asm.py
+++ dg_asm_examples/superstack/superstack_to_asm.py
@@ -4,79 +4,153 @@ import random
 import click
 import sys
 import os
+import functools
 
 def get_superstack_parser():
     """
     Returns the complete brainfuck parser --> Digirule ASM.
     """
-    # def _get_label_tag(tag_chars="0123456789", N=12):
-        # return "".join([tag_chars[random.randint(0,len(tag_chars)-1)] for n in range(N)])
+    def _get_label_tag(tag_chars="0123456789", N=12):
+        return "".join([tag_chars[random.randint(0,len(tag_chars)-1)] for n in range(N)])
         
-    # def inc_dp(s, loc, toks):
-        # reps = len(toks[0][0])
-        # if reps>1:
-            # return f"COPYRA dp\nADDLA {reps}\nCOPYAR dp\n" 
-        # else:
-            # return f"INCR dp\n"
-    
-    # def dec_dp(s, loc, toks):
-        # reps = len(toks[0][0])
-        # if reps>1:
-            # return f"COPYRA dp\nSUBLA {reps}\nCOPYAR dp\n" 
-        # else:
-            # return f"DECR dp\n"
-            
-    # def inc_dv(s, loc, toks):
-        # return f"COPYIA dp\nADDLA {len(toks[0][0])}\nCOPYAI dp\n" 
+    def _push_literal(s, loc, toks):
+        try:
+            num = int(toks[0][0]) & 0xFF
+        except ValueError:
+            pass
+        return {"statements":f"COPYLR {num} head_val\nCALL f_push\n", "dependencies":set()}
         
-    # def dec_dv(s, loc, toks):
-        # return f"COPYIA dp\nSUBLA {len(toks[0][0])}\nCOPYAI dp\n"        
+    def _add(s, loc, toks):
+        return {"statements":"CALL f_pop\nCOPYRA head_val\nCALL f_pop\nADDRA head_val\nCOPYAR head_val\nCALL f_push\n",
+                "dependencies":set()}
     
-    # def out_dv(s, loc, toks):
-        # return f"COPYIR dp out_dev\n"
+    def _sub(s, loc, toks):
+        return {"statements":"CALL f_pop\nCOPYRA head_val\nCALL f_pop\nSUBRA head_val\nCOPYAR head_val\nCALL f_push\n",
+                "dependencies":set()}
+
+    def _mul(s, loc, toks):
+        return {"statements":"CALL f_pop\nCOPYRR head_val head_val_1\nCALL f_pop\nMUL head_val head_val_1\nCALL f_push\n",
+                "dependencies":set()}
+
+    def _div(s, loc, toks):
+        return {"statements":"CALL f_pop\nCOPYRR head_val head_val_1\nCALL f_pop\nDIV head_val head_val_1\nCALL f_push\n",
+                "dependencies":set()}
+
+    def _mod(s, loc, toks):
+        return {"statements":"CALL f_pop\nCOPYRR head_val head_val_1\nCALL f_pop\nDIV head_val head_val_1\nCOPYAR head_val\nCALL f_push\n",
+                "dependencies":set()}
+
+    def _random(s, loc, toks):
+        return {"statements":"CALL f_rand\nCALL f_push\n",
+                "dependencies":{"f_rand"}}
+
+    def _and(s, loc, toks):
+        return {"statements":"CALL f_pop\nCOPYRA head_val\nCALL f_pop\nANDRA head_val\nCOPYAR head_val\nCALL f_push\n",
+                "dependencies":set()}
+
+    def _or(s, loc, toks):
+        return {"statements":"CALL f_pop\nCOPYRA head_val\nCALL f_pop\nORRA head_val\nCOPYAR head_val\nCALL f_push\n",
+                "dependencies":set()}
+
+    def _xor(s, loc, toks):
+        return {"statements":"CALL f_pop\nCOPYRA head_val\nCALL f_pop\nXORRA head_val\nCALL f_push\n",
+                "dependencies":set()}
+
+    def _output(s, loc, toks):
+        return {"statements":"CALL f_pop\nCOPYRR head_val out_dev\n",
+                "dependencies":set()}
+
+    def _input(s, loc, toks):
+        return {"statements":"COPYRR in_dev head_val\nCALL f_push\n",
+                "dependencies":set()}
+
+    def _inputascii(s, loc, toks):
+        return {"statements": "COMIN\nCOPYAR head_val\nCALL f_push\n",
+                "dependencies": set()}
+
+    def _pop(s, loc, toks):
+        return {"statements": "CALL f_pop\n",
+                "dependencies": set()}
+
+    def _swap(s, loc, toks):
+        return {"statements": "CALL f_pop\nCOPYRA head_val\nCALL f_pop\nSWAPRA head_val\nCALL f_push\nCOPYAR head_val\nCALL f_push\n",
+                "dependencies": set()}
+
+    def _dup(s, loc, toks):
+        return {"statements": "CALL f_pop\nCALL f_push\nCALL f_push\n",
+                "dependencies": set()}
+
+    def _rev(s, loc, toks):
+        return {"statements": "CALL f_rev\n", 
+                "dependencies": {"f_rev"}}
+
+    def _quit(s, loc, toks):
+        return {"statements":"HALT\n",
+                "dependencies": set()}
+        
+    def _iteration_block(s, loc, toks):
+        label_tag = _get_label_tag()
+        return {"statements":f"label_{label_tag}:\n{''.join([s['statements'] for s in toks[0][1:-1]])}CALL f_peek\nCOPYRA head_val\nBCRSS zero_bit status_reg\nJUMP label_{label_tag}\n", 
+                "dependencies":functools.reduce(lambda x,y:x.union(y["dependencies"]), toks[0][1:-1], set()).union({"f_peek"})}
+
+    def _cycle(s, loc, toks):
+        return {"statements": f"DECR head_ptr\nCOPYRR head_ptr head_val\nCOPYLR stack head_val_1\nCOPYLR 10 f_custom_ins\nCALL f_custom_ins\nINCR head_ptr\n",
+                "dependencies": set()}
+
+    def _rcycle(s, loc, toks):
+        return {"statements": f"DECR head_ptr\nCOPYRR head_ptr head_val_1\nCOPYLR stack head_val\nCOPYLR 10 f_custom_ins\nCALL f_custom_ins\nINCR head_ptr\n",
+                "dependencies": set()}
+    
+        
+    def _emit_asm(s, loc, toks):
+        precode = ".EQU status_reg=252\n.EQU in_dev=253\n.EQU out_dev=255\n.EQU zero_bit=0\nCOPYLR stack head_ptr\nstart_program:\n"
+        postcode = "f_push:\nCOPYRI head_val head_ptr\nINCR head_ptr\nRETURN\n\nf_pop:\nDECR head_ptr\nCOPYIR head_val head_ptr\nRETURN\n\nf_custom_ins:\n.DB 0\nhead_val:\n.DB 0\nhead_val_1:\n.DB 0\nRETURN\nhead_ptr:\n.DB 0\nstack:\n.DB 0\n"
+        deps_code = {"f_rand": "f_rand:\nRANDA\nCOPYAR head_val_1\nSUBRA head_val\nBCRSC carry_bit status_reg\nJUMP f_rand\nCOPYRA head_val\nRETURN\n", 
+                     "f_rev":"f_rev:\nCOPYRA head_ptr\nSUBLA stack\nCOPYRR head_ptr head_val # Save the head pointer\nCOPYLR stack head_val_1\nCOPYLR 16 f_custom_ins # SWAPRR\nswap_again:\nCALL f_custom_ins\nSUBLA 1\nBCRSS zero_bit status_reg\nJUMP f_rev_adjust_and_loop\nRETURN\nf_rev_adjust:\nINCR head_val\nDECR head_val_1\nJUMP swap_again\n",
+                     "f_peek":"f_peek:\nDECR head_ptr\nCOPYIR head_ptr head_val\nINCR head_ptr\nRETURN\n"}
+        # Collect dependencies and leave just compiled code
+        deps = set()
+        for a_tok in toks:
+            deps = deps.union(a_tok["dependencies"])
                 
-    # def in_dv(s, loc, toks):
-        # return f"COPYRI in_dev dp\n"
+        return precode + \
+               f"{''.join([m['statements'] for m in toks])}" +\
+               "HALT\n" + \
+               "".join([deps_code[k] for k in deps])+ \
+               postcode
         
-    # def iteration_block(s, loc, toks):
-        # label_tag = _get_label_tag()
-        # return f"label_{label_tag}:\n{''.join(toks[0][1:-1])}COPYIA dp\nBCRSS zero_bit status_reg\nJUMP label_{label_tag}\n"
-        
-    # def emit_asm(s, loc, toks):
-        # return f".EQU status_reg=252\n.EQU in_dev=253\n.EQU out_dev=255\n.EQU zero_bit=0\nCOPYLR tape dp\nstart_program:\n" \
-               # f"{''.join([m for m in toks])}HALT\ndp:\n.DB 0\ntape:\n.DB 0"
-        
-    sust_literal = pyparsing.Group(pyparsing.Regex("[0-9]+"))("LITERAL")
-    sust_add = pyparsing.Group(pyparsing.Regex("[Aa][Dd][Dd]"))("ADD")
-    sust_sub = pyparsing.Group(pyparsing.Regex("[Ss][Uu][Bb]"))("SUB")
-    sust_mul = pyparsing.Group(pyparsing.Regex("[Mm][Uu][Ll]"))("MUL")
-    sust_div = pyparsing.Group(pyparsing.Regex("[Dd][Ii][Vv]"))("DIV")
-    sust_mod = pyparsing.Group(pyparsing.Regex("[Mm][Oo][Dd]"))("MOD")
-    sust_random = pyparsing.Group(pyparsing.Regex("[Rr][Aa][Nn][Dd][Oo][Mm]"))("RANDOM")
-    sust_and = pyparsing.Group(pyparsing.Regex("[Aa][Nn][Dd]"))("AND")
-    sust_or = pyparsing.Group(pyparsing.Regex("[Oo][Rr]"))("OR")
-    sust_xor = pyparsing.Group(pyparsing.Regex("[Xx][Oo][Rr]"))("XOR")
+    sust_literal = pyparsing.Group(pyparsing.Regex("[0-9]+"))("LITERAL").setParseAction(_push_literal)
+    sust_add = pyparsing.Group(pyparsing.Regex("[Aa][Dd][Dd]"))("ADD").setParseAction(_add)
+    sust_sub = pyparsing.Group(pyparsing.Regex("[Ss][Uu][Bb]"))("SUB").setParseAction(_sub)
+    sust_mul = pyparsing.Group(pyparsing.Regex("[Mm][Uu][Ll]"))("MUL").setParseAction(_mul)
+    sust_div = pyparsing.Group(pyparsing.Regex("[Dd][Ii][Vv]"))("DIV").setParseAction(_div)
+    sust_mod = pyparsing.Group(pyparsing.Regex("[Mm][Oo][Dd]"))("MOD").setParseAction(_mod)
+    sust_random = pyparsing.Group(pyparsing.Regex("[Rr][Aa][Nn][Dd][Oo][Mm]"))("RANDOM").setParseAction(_random)
+    sust_and = pyparsing.Group(pyparsing.Regex("[Aa][Nn][Dd]"))("AND").setParseAction(_and)
+    sust_or = pyparsing.Group(pyparsing.Regex("[Oo][Rr]"))("OR").setParseAction(_or)
+    sust_xor = pyparsing.Group(pyparsing.Regex("[Xx][Oo][Rr]"))("XOR").setParseAction(_xor)
     sust_nand = pyparsing.Group(pyparsing.Regex("[Nn][Aa][Nn][Dd]"))("NAND")
     sust_not = pyparsing.Group(pyparsing.Regex("[Nn][Oo][Tt]"))("NOT")
-    sust_output = pyparsing.Group(pyparsing.Regex("[Oo][Uu][Tt][Pp][Uu][Tt]"))("OUTPUT")
-    sust_input = pyparsing.Group(pyparsing.Regex("[Ii][Nn][Pp][Uu][Tt]"))("INPUT")
+    sust_output = pyparsing.Group(pyparsing.Regex("[Oo][Uu][Tt][Pp][Uu][Tt]"))("OUTPUT").setParseAction(_output)
+    sust_input = pyparsing.Group(pyparsing.Regex("[Ii][Nn][Pp][Uu][Tt]"))("INPUT").setParseAction(_input)
     sust_outputascii = pyparsing.Group(pyparsing.Regex("[Oo][Uu][Tt][Pp][Uu][Tt][Aa][Ss][Cc][Ii]"))("OUTPUT_ASCII")
-    sust_inputascii = pyparsing.Group(pyparsing.Regex("[Ii][Nn][Pp][Uu][Tt][Aa][Ss][Cc][Ii]"))("INPUT_ASCII")
-    sust_pop = pyparsing.Group(pyparsing.Regex("[Pp][Oo][Pp]"))("POP")
-    sust_swap = pyparsing.Group(pyparsing.Regex("[Ss][Ww][Aa][Pp]"))("SWAP")
-    sust_cycle = pyparsing.Group(pyparsing.Regex("[Cc][Yy][Cc][Ll][Ee]"))("CYCLE")
-    sust_rcycle = pyparsing.Group(pyparsing.Regex("[Rr][Cc][Yy][Cc][Ll][Ee]"))("RCYCLE")
-    sust_dup = pyparsing.Group(pyparsing.Regex("[Dd][Uu][Pp]"))("DUP")
-    sust_rev = pyparsing.Group(pyparsing.Regex("[Rr][Ee][Vv]"))("REV")
-    sust_quit = pyparsing.Group(pyparsing.Regex("[Qq][Uu][Ii][Tt]"))("QUIT")
+    sust_inputascii = pyparsing.Group(pyparsing.Regex("[Ii][Nn][Pp][Uu][Tt][Aa][Ss][Cc][Ii]"))("INPUT_ASCII").setParseAction(_inputascii)
+    sust_pop = pyparsing.Group(pyparsing.Regex("[Pp][Oo][Pp]"))("POP").setParseAction(_pop)
+    sust_swap = pyparsing.Group(pyparsing.Regex("[Ss][Ww][Aa][Pp]"))("SWAP").setParseAction(_swap)
+    sust_cycle = pyparsing.Group(pyparsing.Regex("[Cc][Yy][Cc][Ll][Ee]"))("CYCLE").setParseAction(_cycle)
+    sust_rcycle = pyparsing.Group(pyparsing.Regex("[Rr][Cc][Yy][Cc][Ll][Ee]"))("RCYCLE").setParseAction(_rcycle)
+    sust_dup = pyparsing.Group(pyparsing.Regex("[Dd][Uu][Pp]"))("DUP").setParseAction(_dup)
+    sust_rev = pyparsing.Group(pyparsing.Regex("[Rr][Ee][Vv]"))("REV").setParseAction(_rev)
+    sust_quit = pyparsing.Group(pyparsing.Regex("[Qq][Uu][Ii][Tt]"))("QUIT").setParseAction(_quit)
     sust_debug = pyparsing.Group(pyparsing.Regex("[Dd][Ee][Bb][Uu][Gg]"))("DEBUG")
     sust_statement = pyparsing.Forward()
-    sust_if_block = pyparsing.Group(pyparsing.Regex("[Ii][Ff]")+pyparsing.ZeroOrMore(sust_statement)+pyparsing.Regex("[Ff][Ii]"))
+    sust_if_block = pyparsing.Group(pyparsing.Regex("[Ii][Ff]")+pyparsing.ZeroOrMore(sust_statement)+pyparsing.Regex("[Ff][Ii]")).setParseAction(_iteration_block)
     sust_statement << (sust_literal ^ sust_add ^ sust_sub ^ sust_mul ^ sust_div ^ sust_mod ^ sust_random ^ sust_and ^
                        sust_or ^ sust_xor ^ sust_nand ^ sust_not ^ sust_output ^ sust_input ^ sust_outputascii ^
                        sust_inputascii ^ sust_pop ^ sust_swap ^ sust_cycle ^ sust_rcycle ^ sust_dup ^ sust_rev ^  
                        sust_quit ^ sust_debug ^ sust_if_block)
-    sust_program = pyparsing.OneOrMore(sust_statement)
+    sust_program = pyparsing.OneOrMore(sust_statement).setParseAction(_emit_asm)
+    sust_program.ignore(sust_debug)
     return sust_program
     
     
@@ -85,9 +159,9 @@ def get_superstack_parser():
 def main(input_file):
     sust2asm_parser = get_superstack_parser()
     try:
-        parsed_pro = sust2asm_parser.parseString(input_file.read(), parseAll=True)
-        # dg_asm_text = bf2asm_parser.parseString(input_file.read(), parseAll=True)
-        # sys.stdout.write(dg_asm_text[0])
+        # parsed_pro = sust2asm_parser.parseString(input_file.read(), parseAll=True)
+        dg_asm_text = sust2asm_parser.parseString(input_file.read(), parseAll=True)
+        sys.stdout.write(dg_asm_text[0])
     except pyparsing.ParseException as pe:
         print(str(pe))
         sys.exit(-1)
