100aeca0a80b76b0be0a90cecac3de4e1c2ecdbb
Athanasios Anastasiou <athanastasiou@gmail.com>
2020-08-09 01:59:10 +0100

    Included the reversal in the parseAction

--- dg_asm_examples/sethi_ullman/exp2asm.py
+++ dg_asm_examples/sethi_ullman/exp2asm.py
@@ -3,32 +3,13 @@ TODO: Can compile down to a single expression or an autonomous program. Better t
 """
 import pyparsing
 
-def gen_code(current_expression):
-    # Translate table
-    ops_lookup = {"*":"MUL",
-                  "/":"DIV",
-                  "%":"MOD",
-                  "+":"SUM",
-                  "-":"SUB"}
-    to_ret =[]  
-    k = 0
-    while k<len(current_expression):
-        a_tok = current_expression[k]
-        if type(a_tok) is str and a_tok in ops_lookup:
-            to_ret.append(current_expression[k+1])
-            to_ret.append(ops_lookup[current_expression[k]])
-            k+=1
-        else:
-            to_ret.append(a_tok)
-        k+=1
-    to_ret_2 = []
-    for a_tok in to_ret:
-        if type(a_tok) is pyparsing.ParseResults:
-            to_ret_2.extend(gen_code(a_tok))
-        else:
-            to_ret_2.append(a_tok)
-    return to_ret_2
-    
+def rev_ops(s,loc,toks):
+    ops_lookup = {"+":"add",
+                  "-":"sub",
+                  "*":"mul",
+                  "/":"div",
+                  "%":"mod"}
+    return toks[0][0::2],ops_lookup[toks[0][1]]
     
 def get_parser():
     """
@@ -45,8 +26,8 @@ def get_parser():
     logic_exp = pyparsing.Forward()
     identifier = pyparsing.Regex("[a-zA-Z_][a-zA-Z0-9_]*")
     operand = literal^identifier
-    numeric_exp = pyparsing.infixNotation(operand,[(pyparsing.oneOf('* / %'),2,pyparsing.opAssoc.LEFT), 
-                                                   (pyparsing.oneOf('+ -'),2,pyparsing.opAssoc.LEFT)])
+    numeric_exp = pyparsing.infixNotation(operand,[(pyparsing.oneOf('* / %'),2,pyparsing.opAssoc.LEFT, rev_ops), 
+                                                   (pyparsing.oneOf('+ -'),2,pyparsing.opAssoc.LEFT, rev_ops)])
     assignment_statement = pyparsing.Group(identifier("idf") + "=" + numeric_exp("nexp"))("STATEMENT")
     numeric_program = pyparsing.OneOrMore(assignment_statement)
     # return numeric_program
@@ -55,13 +36,12 @@ def get_parser():
 if __name__ == "__main__":    
     numeric_exp = get_parser()
     # exp_stack = [numeric_exp.parseString("(3*(4/(6+a)+b)+c)*(6*(2+a))")]
-    exp_stack = [numeric_exp.parseString("2*a+4*b+6*j")]
+    exp_stack = numeric_exp.parseString("(3*(4/(6+a)+b)+c)*(6*(2+a))")
     # exp_stack = [numeric_exp.parseString("a")]
     
     # Working on this
     # exp_stack = [numeric_exp.parseString("a=2 b=4 x=2 c=0 y=a*x*x+b*x+c")]
     
-    v = gen_code(exp_stack)
     # while len(exp_stack):
         # current_exp = exp_stack.pop()
         # print(current_exp)
