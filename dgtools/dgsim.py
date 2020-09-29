b7922e73bba34095680e459493a607043cf1bda4
Athanasios Anastasiou <athanastasiou@gmail.com>
2020-09-30 00:41:47 +0100

    Added the set pixel

--- dgtools/dgsim.py
+++ dgtools/dgsim.py
@@ -143,9 +143,16 @@ def trace_program(program, output_file, skip_n=0, max_n=200, trace_title="",
                         # frame_data.append(0)
                         # frame_data.append(0)
                         
-                    frames.append(Image.frombytes(size=(16,16),
-                                                  data=bytes([u for u in machine._mem]),
-                                                  mode="L").resize((128, 128), resample=Image.NEAREST))
+                    # frames.append(Image.frombytes(size=(16,16),
+                                                  # data=bytes([u for u in machine._mem]),
+                                                  # mode="L").resize((128, 128), resample=Image.NEAREST))
+                    frame_data = []
+                    for u in machine._mem:
+                        for r in range(0,8):
+                            frame_data.append(255 if (u & (1<<r)) else 128)
+                    frames.append(Image.frombytes(size=(64,32),
+                                                  data=bytes(frame_data),
+                                                  mode="L").resize((128,64),resample=Image.NEAREST))
                 # Machine registers
                 dgen.open_tag("section")
                 dgen.named_anchor(f"n{n}")
